b743e1dc3d1fb78919304134c081be6d4bfd4835
ravier1 <rvgk1202@icloud.com>
2024-02-11 15:26:28 -0500

    Worked on settin up taking the commands, bit like an array( Excuse me for horrible code, I am new to discord js)

--- index.js
+++ index.js
@@ -7,3 +7,23 @@ const { Player } = require("discord-player");
 
 const fs = require("node.fs");
 const path = require("node:path");
+
+const client = new Client({
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES]
+});
+
+//Loadingg all the commmands
+const commands = [];
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"));
+
+for(const file of commandFiles)
+{
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+
+    client.commands.set(command.data.name, command);
+}
+
