ff5a74c1989d69bd32f00d083a253ef6b3721667
ravier1 <rvgk1202@icloud.com>
2024-02-26 15:57:42 -0500

    Finished skip.js. this was too easy because of discord player in the index.js

---
+++ commands/skip.js
@@ -0,0 +1,30 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageEmbed, Message } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("skip")
+        .setDescription("Skips the current song."),
+    execute : async ({client, interaction}) => {
+
+        const queue = client.player.getQueue(interaction.guild);
+
+        if(!queue)
+        {
+            await interaction.reply("There is no song playing currently.");
+            return;
+        }
+
+        const currentSong = queue.current;
+
+        queue.skip();
+
+        await interactionreply({
+            embeds:
+                new MessageEmbed()
+                    .setDescription("Skipped **${currentSong.title}**")
+                    .setThumbnail(currentSong.thumbnail)
+        })
+
+    }
+}
