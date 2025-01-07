968a0cb23f626ba9172abb4d1ace3b33804e696d
Ravi G Kumar <rvgk1202@icloud.com>
2025-01-07 07:48:01 -0500

    Update bot.py

--- beethoven/bot.py
+++ beethoven/bot.py
@@ -1,9 +1,22 @@
 import discord
 from discord.ext import commands
+from discord import FFmpegPCMAudio
+import yt_dlp
 
 # Create the bot
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="!", intents=intents)
+
+# YouTube options
+yt_dlp_opts = {
+    "format": "bestaudio/best",
+    "quiet": True,
+    "postprocessors": [{
+        "key": "FFmpegExtractAudio",
+        "preferredcodec": "mp3",
+        "preferredquality": "192",
+    }],
+}
 
 # I am back! (This is a comment)
 @bot.event
@@ -18,7 +31,54 @@ async def ping_prefix(ctx):
 # Slash command: /ping
 @bot.tree.command(name="ping", description="Replies with Pong!")
 async def ping_slash(interaction: discord.Interaction):
-    await interaction.response.send_message("Pong! (Slash)")
+    await interaction.response.send_message("Pong!")
+
+
+# Join voice channel
+@bot.command(name="join")
+async def join(ctx):
+    if ctx.author.voice:
+        channel = ctx.author.voice.channel
+        await channel.connect()
+        await ctx.send(f"Joined {channel.name}!")
+    else:
+        await ctx.send("You need to be in a voice channel for me to join.")
+
+
+@bot.command(name="play")
+async def play(ctx, url: str):
+    # If the bot isn't connected, join the user's voice channel
+    if not ctx.voice_client:
+        if ctx.author.voice:
+            channel = ctx.author.voice.channel
+            await channel.connect()
+            await ctx.send(f"Joined {channel.name}!")
+        else:
+            await ctx.send("You need to be in a voice channel to use this command.")
+            return
+
+    vc = ctx.voice_client
+
+    # Download and play audio
+    await ctx.send("Fetching audio...")
+    with yt_dlp.YoutubeDL(yt_dlp_opts) as ydl:
+        info = ydl.extract_info(url, download=False)
+        audio_url = info["url"]
+
+    vc.stop()  # Stop any currently playing audio
+    vc.play(FFmpegPCMAudio(audio_url), after=lambda e: print(f"Finished playing: {e}"))
+    await ctx.send(f"Now playing: {info['title']}")
+
+
+# Leave voice channel
+@bot.command(name="leave")
+async def leave(ctx):
+    if ctx.voice_client:
+        await ctx.voice_client.disconnect()
+        await ctx.send("Bye!")
+    else:
+        await ctx.send("I'm not connected to any voice channel.")
+
 
 # setup_hook is a special method that is called when the bot is starting up.
 # It is necessary to sync the slash commands with Discord so that they are registered and available for use.
