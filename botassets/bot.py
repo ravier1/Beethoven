3ebafafed0ce06528080695c27d95a7dc03abefb
Ravi G Kumar <rvgk1202@icloud.com>
2025-01-08 10:04:48 -0500

    Volume HotFix

--- botassets/bot.py
+++ botassets/bot.py
@@ -1,11 +1,5 @@
 import discord
 from discord.ext import commands
-<<<<<<< Updated upstream:beethoven/bot.py
-
-# Create the bot
-intents = discord.Intents.all()
-bot = commands.Bot(command_prefix="!", intents=intents)
-=======
 import os
 import json
 
@@ -23,31 +17,12 @@ bot = commands.Bot(
     intents=intents,
     application_id=config["APPLICATION_ID"]
 )
->>>>>>> Stashed changes:botassets/bot.py
 
 # I am back! (This is a comment)
 @bot.event
 async def on_ready():
     print(f"{bot.user.name} is connected and ready!")
 
-<<<<<<< Updated upstream:beethoven/bot.py
-# Prefix command: !ping
-@bot.command(name="ping")
-async def ping_prefix(ctx):
-    await ctx.send("Pong! (Prefix)")
-
-# Slash command: /ping
-@bot.tree.command(name="ping", description="Replies with Pong!")
-async def ping_slash(interaction: discord.Interaction):
-    await interaction.response.send_message("Pong! (Slash)")
-
-# setup_hook is a special method that is called when the bot is starting up.
-# It is necessary to sync the slash commands with Discord so that they are registered and available for use.
-@bot.event
-async def setup_hook():
-    await bot.tree.sync()
-    print("Slash commands synced!")
-=======
 # Load all commands dynamically from the commands folder
 async def load_commands():
     for filename in os.listdir("./botassets/commands"):
@@ -61,5 +36,4 @@ async def load_commands():
 async def setup():
     await load_commands()
     await bot.load_extension("botassets.slash_commands")
->>>>>>> Stashed changes:botassets/bot.py
 
--- run.py
+++ run.py
@@ -1,10 +1,6 @@
 import json
-<<<<<<< Updated upstream
-from beethoven.bot import bot
-=======
 import asyncio
 from botassets.bot import bot, setup
->>>>>>> Stashed changes
 
 # Load config
 with open("config.json") as f:
